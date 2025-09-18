3fa1429c8b8075453bd58e55251dc94ae2979df6
AlgoGenex <algogenex@gmail.com>
2025-09-18 17:26:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-import { createChat } from 'https://www.algogenex.com/n8n-chat-styles/chat.bundle.es.js';
-export { createChat };
+// script.js
+
+import { createChat } from "https://www.algogenex.com/n8n-chat-styles/chat.bundle.es.js";
+
+export function initChat(container) {
+  // createChat should accept a container or mount point
+  createChat({
+    target: container // <-- IMPORTANT: mount into shadow DOM
+  });
+}
